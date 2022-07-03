c7119cd636dd710763cf306f26b3838533818b39
josergdev <joserodriguezgarcia95@gmail.com>
2022-07-03 13:19:11 +0200

    Add top quality limit 50 test

--- tests/GildedRoseTest.php
+++ tests/GildedRoseTest.php
@@ -55,4 +55,25 @@ class GildedRoseTest extends TestCase
             $this->assertGreaterThanOrEqual(0, $item->quality, $item->__toString());
         }
     }
+
+    public function test_it_should_not_increase_quality_of_any_item_more_than_fifty(): void
+    {
+        $items = [
+            new Item('Normal Item', 2, 49),
+            new Item('Normal Item', -2, 50),
+            new Item('Aged Brie', 2, 49),
+            new Item('Aged Brie', -2, 50),
+            new Item('Sulfuras', 2, 50),
+            new Item('Backstage passes', 0, 50),
+            new Item('Backstage passes', 5, 50),
+            new Item('Backstage passes', 10, 50),
+        ];
+        $gildedRose = new GildedRose($items);
+
+        $gildedRose->updateQuality();
+
+        foreach ($items as $item) {
+            $this->assertLessThanOrEqual(50, $item->quality, $item->__toString());
+        }
+    }
 }
