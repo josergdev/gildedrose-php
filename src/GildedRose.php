569860a0374928d807634c301bc1f06726370448
josergdev <joserodriguezgarcia95@gmail.com>
2022-07-03 15:57:17 +0200

    Split all behaviour by item type

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -33,12 +33,33 @@ final class GildedRose
             if ($item->quality > 0) {
                 $item->quality = $item->quality - 1;
             }
+
+            $item->sell_in = $item->sell_in - 1;
+
+            if ($item->sell_in >= 0) {
+                return;
+            }
+
+            if ($item->quality > 0) {
+                $item->quality = $item->quality - 1;
+            }
         }
 
         if ($this->isAgedBrieItem($item)) {
             if ($item->quality < 50) {
                 $item->quality = $item->quality + 1;
             }
+
+            $item->sell_in = $item->sell_in - 1;
+
+            if ($item->sell_in >= 0) {
+                return;
+            }
+
+            if ($item->quality < 50) {
+                $item->quality = $item->quality + 1;
+            }
+            return;
         }
 
         if ($this->isBackstageItem($item)) {
@@ -57,28 +78,14 @@ final class GildedRose
                     }
                 }
             }
-        }
 
-        $item->sell_in = $item->sell_in - 1;
+            $item->sell_in = $item->sell_in - 1;
 
-        if ($item->sell_in >= 0) {
-            return;
-        }
-
-        if ($item->name === 'Aged Brie') {
-            if ($item->quality < 50) {
-                $item->quality = $item->quality + 1;
+            if ($item->sell_in >= 0) {
+                return;
             }
-            return;
-        }
 
-        if ($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
             $item->quality = 0;
-            return;
-        }
-
-        if ($item->quality > 0) {
-            $item->quality = $item->quality - 1;
         }
     }
 
