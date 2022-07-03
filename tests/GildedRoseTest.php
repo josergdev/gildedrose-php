404fd54096b14dfe577603a49134e02819c39581
josergdev <joserodriguezgarcia95@gmail.com>
2022-07-03 13:02:01 +0200

    Add decreasing quality tests

--- tests/GildedRoseTest.php
+++ tests/GildedRoseTest.php
@@ -10,11 +10,49 @@ use PHPUnit\Framework\TestCase;
 
 class GildedRoseTest extends TestCase
 {
-    public function testFoo(): void
+
+    public function test_it_should_decrease_quality_of_normal_items_in_one_point(): void
     {
-        $items = [new Item('foo', 0, 0)];
+        $items = [new Item('Normal Item', 10, 10)];
         $gildedRose = new GildedRose($items);
+
         $gildedRose->updateQuality();
-        $this->assertSame('foo', $items[0]->name);
+
+        $this->assertSame('Normal Item', $items[0]->name);
+        $this->assertSame(9,  $items[0]->sell_in);
+        $this->assertSame(9,  $items[0]->quality);
+    }
+
+    public function test_it_should_decrease_quality_of_normal_items_in_two_points_if_sell_date_has_passed(): void
+    {
+        $items = [new Item('Normal Item', 0, 10)];
+        $gildedRose = new GildedRose($items);
+
+        $gildedRose->updateQuality();
+
+        $this->assertSame('Normal Item', $items[0]->name);
+        $this->assertSame(-1,  $items[0]->sell_in);
+        $this->assertSame(8,  $items[0]->quality);
+    }
+
+    public function test_it_should_not_decrease_quality_of_any_item_lower_than_zero(): void
+    {
+        $items = [
+            new Item('Normal Item', 0, 0),
+            new Item('Normal Item', 2, 0),
+            new Item('Normal Item', -2, 0),
+            new Item('Aged Brie', 2, 0),
+            new Item('Sulfuras', 2, 0),
+            new Item('Backstage passes', 0, 0),
+            new Item('Backstage passes', 5, 0),
+            new Item('Backstage passes', 10, 0),
+        ];
+        $gildedRose = new GildedRose($items);
+
+        $gildedRose->updateQuality();
+
+        foreach ($items as $item) {
+            $this->assertGreaterThanOrEqual(0, $item->quality, $item->__toString());
+        }
     }
 }
