b511159cb8224929efb08b2811b0e9838d9785e3
josergdev <joserodriguezgarcia95@gmail.com>
2022-07-03 15:19:38 +0200

    flip if

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -53,20 +53,23 @@ final class GildedRose
 
         $item->sell_in = $item->sell_in - 1;
 
-        if ($item->sell_in < 0) {
-            if ($item->name != 'Aged Brie') {
-                if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($item->quality > 0) {
-                        $item->quality = $item->quality - 1;
-                    }
-                } else {
-                    $item->quality = $item->quality - $item->quality;
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                }
+        if ($item->sell_in >= 0) {
+            return;
+        }
+
+        if ($item->name == 'Aged Brie') {
+            if ($item->quality < 50) {
+                $item->quality = $item->quality + 1;
             }
+            return;
+        }
+
+        if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if ($item->quality > 0) {
+                $item->quality = $item->quality - 1;
+            }
+        } else {
+            $item->quality = $item->quality - $item->quality;
         }
     }
 }
