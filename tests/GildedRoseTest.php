1276b8c076a0aef7c10bc5848c8917bfa7359836
josergdev <joserodriguezgarcia95@gmail.com>
2022-07-03 14:39:22 +0200

    Add backstage test cases

--- tests/GildedRoseTest.php
+++ tests/GildedRoseTest.php
@@ -106,15 +106,17 @@ class GildedRoseTest extends TestCase
 
     public function provideSulfurasCases(): array
     {
+        $sulfuras = 'Sulfuras, Hand of Ragnaros';
+
         return [
-            ['Sulfuras, Hand of Ragnaros, 10, 50', new Item('Sulfuras, Hand of Ragnaros', 10, 50)],
-            ['Sulfuras, Hand of Ragnaros, 5, 50', new Item('Sulfuras, Hand of Ragnaros', 5, 50)],
-            ['Sulfuras, Hand of Ragnaros, 0, 50', new Item('Sulfuras, Hand of Ragnaros', 0, 50)],
-            ['Sulfuras, Hand of Ragnaros, -5, 50', new Item('Sulfuras, Hand of Ragnaros', -5, 50)],
-            ['Sulfuras, Hand of Ragnaros, 10, 45', new Item('Sulfuras, Hand of Ragnaros', 10, 45)],
-            ['Sulfuras, Hand of Ragnaros, 5, 45', new Item('Sulfuras, Hand of Ragnaros', 5, 45)],
-            ['Sulfuras, Hand of Ragnaros, 0, 45', new Item('Sulfuras, Hand of Ragnaros', 0, 45)],
-            ['Sulfuras, Hand of Ragnaros, -5, 45', new Item('Sulfuras, Hand of Ragnaros', -5, 45)],
+            ["$sulfuras, 10, 50", new Item($sulfuras, 10, 50)],
+            ["$sulfuras, 5, 50", new Item($sulfuras, 5, 50)],
+            ["$sulfuras, 0, 50", new Item($sulfuras, 0, 50)],
+            ["$sulfuras, -5, 50", new Item($sulfuras, -5, 50)],
+            ["$sulfuras, 10, 45", new Item($sulfuras, 10, 45)],
+            ["$sulfuras, 5, 45", new Item($sulfuras, 5, 45)],
+            ["$sulfuras, 0, 45", new Item($sulfuras, 0, 45)],
+            ["$sulfuras, -5, 45", new Item($sulfuras, -5, 45)],
         ];
     }
 
@@ -124,6 +126,38 @@ class GildedRoseTest extends TestCase
     public function test_it_should_not_decrease_quality_or_sell_in_when_it_is_sulfuras(string $expectedOutput, Item $inputSulfurasItem): void
     {
         $items = [$inputSulfurasItem];
+        $gildedRose = new GildedRose($items);
+
+        $gildedRose->updateQuality();
+
+        $this->assertSame($expectedOutput, $items[0]->__toString());
+    }
+
+
+    public function provideBackstageCases()
+    {
+        $backStage = 'Backstage passes to a TAFKAL80ETC concert';
+
+        return
+        [
+            ["$backStage, 10, 31", new Item($backStage, 11, 30)],
+            ["$backStage, 9, 32", new Item($backStage, 10, 30)],
+            ["$backStage, 8, 32", new Item($backStage, 9, 30)],
+            ["$backStage, 5, 32", new Item($backStage, 6, 30)],
+            ["$backStage, 4, 33", new Item($backStage, 5, 30)],
+            ["$backStage, 3, 33", new Item($backStage, 4, 30)],
+            ["$backStage, 0, 33", new Item($backStage, 1, 30)],
+            ["$backStage, -1, 0", new Item($backStage, 0, 30)],
+            ["$backStage, -2, 0", new Item($backStage, -1, 0)],
+        ];
+    }
+
+    /**
+     * @dataProvider provideBackstageCases
+     */
+    public function test_it_should_update_quality_when_it_is_backstage_depending_on_sell_in(string $expectedOutput, Item $inputBackstageItem): void
+    {
+        $items = [$inputBackstageItem];
         $gildedRose = new GildedRose($items);
 
         $gildedRose->updateQuality();
